58a2ea3c49b2e629a2d1c6b236e4c959ad86b370
Oscar GS <oscargs.dev@gmail.com>
2025-08-15 20:25:58 -0600

    Basic getting of different scores for 2 different players

--- server.go
+++ server.go
@@ -1,10 +1,25 @@
-package server
+package main
 
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Vane" {
+		return "20"
+	}
+
+	if name == "Oscar" {
+		return "10"
+	}
+
+	return ""
 }
--- server_test.go
+++ server_test.go
@@ -1,4 +1,4 @@
-package server
+package main
 
 import (
 	"net/http"
@@ -15,6 +15,20 @@ func TestGETPlayers(t *testing.T) {
 
 		got := response.Body.String()
 		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns Oscar's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Oscar", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
