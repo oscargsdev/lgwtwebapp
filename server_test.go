d717cd4024db5158423f0ccb9e7b01cd10a3e520
Oscar GS <oscargs.dev@gmail.com>
2025-08-18 20:21:58 -0600

    Starter handler for League endpoint

--- server_test.go
+++ server_test.go
@@ -15,7 +15,7 @@ func TestGETPlayers(t *testing.T) {
 		},
 		nil,
 	}
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("returns Vane's score", func(t *testing.T) {
 		request := newGetScoreResponse("Vane")
@@ -63,7 +63,7 @@ func TestStoreWins(t *testing.T) {
 		map[string]int{},
 		nil,
 	}
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("it records Win on Post", func(t *testing.T) {
 		player := "Vane"
@@ -81,6 +81,20 @@ func TestStoreWins(t *testing.T) {
 		if store.winCalls[0] != player {
 			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
 		}
+	})
+}
+
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := NewPlayerServer(&store)
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
 	})
 }
 
